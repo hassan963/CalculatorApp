ee367cafb06bf463c4bcee17ff711d5cf2407db2
Hassan M Ashraful <hassan@Hassans-Mac.local>
2018-06-29 16:12:26 +0600

    first commit ios

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Hassan M Ashraful on 29/6/18.
+//  Copyright © 2018 Hassan M Ashraful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello dear")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
